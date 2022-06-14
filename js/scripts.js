535558aa808938ee460b110845e7195a76b7050b
pkderlam87 <62060342+pkderlam87@users.noreply.github.com>
2022-06-14 22:48:35 +0200

    Will get the window width and height to show it in the boxes. ref: DOM Manip

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,25 @@
 const widthBox = document.querySelector("#leftBox");
 const heightBox = document.querySelector("#rightBox");
 
-
-window.addEventListener('resize', function (event) {
-    let newWidth = window.innerWidth;
-    let newHeight = window.innerHeight;
-    widthBox.innerHTML = "The width of the window is" + " " + newWidth + " " + "px";
-});
-/*let widthBrowser = window.innerWidth;
-function widthBrowserWindow() {
+let widthBrowser = window.innerWidth;
+widthBox.innerHTML = "The width of the window is" + " " + widthBrowser + " " + "px";
+let heightBrowser = window.innerHeight;
+heightBox.innerHTML = "The height of the window is" + " " + heightBrowser + " " + "px";
+/**
+ * This function get the window width and height if it changes and show the values in the boxes 
+ */
+function widthHeightWindow() {
     let newWidthBrowser = 0;
-    if (widthBrowser !== newWidthBrowser) {
-        let newWidthBrowser = widthBrowser;
-        widthBox.innerHTML = "The width of the window is" + " " + widthBrowser + " " + "px";
+    let newHeightBrowser = 0;
+    if (widthBrowser !== newWidthBrowser || heightBrowser !== newHeightBrowser) {
+        newWidthBrowser = widthBrowser;
+        newHeightBrowser = heightBrowser;
+        window.addEventListener('resize', function (event) {
+            newWidthBrowser = window.innerWidth;
+            newHeightBrowser = window.innerHeight;
+            widthBox.innerHTML = "The width of the window is" + " " + newWidthBrowser + " " + "px";
+            heightBox.innerHTML = "The height of the window is" + " " + newHeightBrowser + " " + "px";
+        });
     }
 }
-widthBrowserWindow();*/
+widthHeightWindow();
