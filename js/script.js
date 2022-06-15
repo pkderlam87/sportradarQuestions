4982757d64259659b37b9c4468c86d8d9b31ea38
pkderlam87 <62060342+pkderlam87@users.noreply.github.com>
2022-06-15 20:59:14 +0200

    Will ranke by score and number of ratings. ref:Arrays-Objects

--- js/script.js
+++ js/script.js
@@ -1,12 +1,16 @@
 import { tvShows } from "./tvShowsBD.js";
 const highestTVShow = document.querySelector(".highest-TV-show");
+const n = 2;
 
 /**
- * This function sort the tvShows array by score
+ * This function sort the tvShows array by score and tiebreaker by numRatings 
  * @param {Array} tvShows- The array of tv shows
  */
 function sortByScore(tvShows) {
     tvShows.sort((a, b) => {
+        if (b.score === a.score) {
+            return b.numRatings - a.numRatings;
+        }
         return b.score - a.score;
     })
     displayHighestTVShow(tvShows);
@@ -14,13 +18,15 @@ function sortByScore(tvShows) {
 sortByScore(tvShows);
 
 /**
- * This function display the tvShows array by score
+ * This function display the two highest tvShows array by score
  * @param {Array} tvShows- The array of tv shows orderly by score and ratings  
  */
 function displayHighestTVShow(tvShows) {
-    for (let i = 0; i < tvShows.length; i++) {
-        highestTVShow.innerHTML += `<h1>Title: ${tvShows[i].title}</h1>
-                                    <h3>Score: ${tvShows[i].score}</h3>
-                                    <h4>Ratings: ${tvShows[i].numRatings}</h4>`;
+    for (let i = 0; i < n; i++) {
+        highestTVShow.innerHTML += `<li>
+                                    <h3>Title: ${tvShows[i].title}</h3>
+                                    <h4>Score: ${tvShows[i].score}</h4>
+                                    <h5>Ratings: ${tvShows[i].numRatings}</h5>
+                                    </li>`;
     }
 }
