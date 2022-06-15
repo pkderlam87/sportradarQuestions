753a084d1e9f6a13c5a14c2408f6080cad3b0991
pkderlam87 <62060342+pkderlam87@users.noreply.github.com>
2022-06-15 10:17:24 +0200

    Will add the functions to disable right-click and left-click in the boxes. ref: DOM Manip

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,11 @@
 const widthBox = document.querySelector("#leftBox");
 const heightBox = document.querySelector("#rightBox");
+const message = document.querySelector("#message");
 
 let widthBrowser = window.innerWidth;
-widthBox.innerHTML = "The width of the window is" + " " + widthBrowser + " " + "px";
+widthBox.innerHTML = `The width of the window is ${widthBrowser} px`;
 let heightBrowser = window.innerHeight;
-heightBox.innerHTML = "The height of the window is" + " " + heightBrowser + " " + "px";
+heightBox.innerHTML = `The height of the window is ${heightBrowser}px`;
 /**
  * This function get the window width and height if it changes and show the values in the boxes 
  */
@@ -17,9 +18,27 @@ function widthHeightWindow() {
         window.addEventListener('resize', function (event) {
             newWidthBrowser = window.innerWidth;
             newHeightBrowser = window.innerHeight;
-            widthBox.innerHTML = "The width of the window is" + " " + newWidthBrowser + " " + "px";
-            heightBox.innerHTML = "The height of the window is" + " " + newHeightBrowser + " " + "px";
+            widthBox.innerHTML = `The width of the window is ${newWidthBrowser} px`;
+            heightBox.innerHTML = `The height of the window is ${newHeightBrowser} px`;
         });
     }
 }
 widthHeightWindow();
+/**
+ * This function disable the right click in the left box and show an alert
+ */
+(function disableRightClick() {
+    widthBox.addEventListener('contextmenu', function (event) {
+        event.preventDefault();
+        alert(`Right Click is disabled in the left box`);
+    })
+})();
+/**
+ * This function disable the left click in the right box and show an alert
+ */
+(function disableLeftClick() {
+    heightBox.addEventListener('click', function (event) {
+        event.preventDefault();
+        alert(`Left Click is disabled in the right box`);
+    })
+})();
